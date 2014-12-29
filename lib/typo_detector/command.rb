4f145a28b40b139def8de9b14af8dd0453f3936a
Masafumi Yokoyama <myokoym@gmail.com>
2014-12-29 19:45:33 +0900

    Extract methods

--- lib/typo_detector/command.rb
+++ lib/typo_detector/command.rb
@@ -7,39 +7,15 @@ module TypoDetector
     def initialize(arguments)
       @options = parse_options(arguments)
       @paths = arguments
+      @word_counts = {}
+      @word_resources = {}
+      @max_width = 0
     end
 
     def run
-      word_counts = {}
-      word_resources = {}
-      max_width = 0
-
-      each_files do |path|
-        words = nil
-        begin
-          words = File.read(path).scan(/\w+/)
-        rescue
-          $stderr.puts("#{$!.message}: <#{path}>")
-          next
-        end
-
-        words.each do |word|
-          word_counts[word] ||= 0
-          word_counts[word] += 1
-          word_resources[word] = path
-        end
-
-        width = words.collect {|word| word.size}.max
-        max_width = width if width > max_width
-      end
-
-      word_counts.select! do |word, count|
-        count == 1
-      end
-
-      word_counts.keys.sort_by {|word, count| word_resources[word]}.each do |word|
-        puts "#{"%#{max_width}s" % word}: #{word_resources[word]}"
-      end
+      index
+      filter
+      show
     end
 
     private
@@ -56,6 +32,39 @@ module TypoDetector
       parser.parse!(arguments)
 
       options
+    end
+
+    def index
+      each_files do |path|
+        words = nil
+        begin
+          words = File.read(path).scan(/\w+/)
+        rescue
+          $stderr.puts("#{$!.message}: <#{path}>")
+          next
+        end
+
+        words.each do |word|
+          @word_counts[word] ||= 0
+          @word_counts[word] += 1
+          @word_resources[word] = path
+        end
+
+        width = words.collect {|word| word.size}.max
+        @max_width = width if width > @max_width
+      end
+    end
+
+    def filter
+      @word_counts.select! do |word, count|
+        count == 1
+      end
+    end
+
+    def show
+      @word_counts.keys.sort_by {|word, count| @word_resources[word]}.each do |word|
+        puts "#{"%#{@max_width}s" % word}: #{@word_resources[word]}"
+      end
     end
 
     def each_files
